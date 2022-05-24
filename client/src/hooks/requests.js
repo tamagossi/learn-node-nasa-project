5d0422ffb7eeea7bbf7dab0fdc6ad796d565be20
Raka Pratama <mgf.prauliyatama@gmail.com>
2022-05-24 09:25:19 +0700

    feat(planet): adding script and request logger
    * adding request logger with morgan
    * adding script at route to run two apps at once

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,6 +1,12 @@
+const API_URL = 'http://localhost:8000';
+
 async function httpGetPlanets() {
-	// TODO: Once API is ready.
-	// Load planets and return as JSON.
+	try {
+		const response = await fetch(`${API_URL}/planets`);
+		return await response.json();
+	} catch (error) {
+		throw new Error(error);
+	}
 }
 
 async function httpGetLaunches() {
--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,24 @@
 const cors = require('cors');
 const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
 const router = require('./routes');
 
 const app = express();
 
+app.use(morgan('combined'));
 app.use(
 	cors({
 		origin: 'http://localhost:3000',
 	})
 );
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../public')));
+
 app.use(router);
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, '../public/', 'index.html'));
+});
 
 module.exports = app;
