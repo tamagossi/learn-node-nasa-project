d453cde30045669663ee78e7df3e7c46c2a3071a
Raka Pratama <mgf.prauliyatama@gmail.com>
2022-05-25 16:44:41 +0700

    test(): add launch controller test

---
+++ server/src/routes/launches/__test__/launches.test.js
@@ -0,0 +1,60 @@
+const app = require('../../../app.js');
+const request = require('supertest');
+
+describe('Test GET /launches', () => {
+	test('It should respond with 200 success', async () => {
+		const response = await request(app).get('/launches');
+		expect(response.statusCode).toBe(200);
+	});
+});
+
+describe('Test POST /launches', () => {
+	const data = {
+		mission: 'USS Enterprise',
+		rocket: 'NCC 1701-D',
+		destination: 'Kepler-168 f',
+		launchDate: 'January 4, 2028',
+	};
+
+	const dataWithoutDate = {
+		mission: 'USS Enterprise',
+		rocket: 'NCC 1701-D',
+		destination: 'Kepler-168 f',
+	};
+
+	const dataWithInvalidDate = {
+		mission: 'USS Enterprise',
+		rocket: 'NCC 1701-D',
+		destination: 'Kepler-168 f',
+		launchDate: 'hai mate',
+	};
+
+	test('It should respond with 201 created', async () => {
+		const response = await request(app).post('/launches').send(data);
+		expect(response.statusCode).toBe(201);
+
+		const requestDate = new Date(data.launchDate).valueOf;
+		const responseDate = response.body.launchDate;
+		expect(responseDate).toBe(requestDate);
+
+		expect(response.body).toMatchObject(dataWithoutDate);
+	});
+
+	test('It should catch missing required properties', async () => {
+		const response = await request(app).post('/launches').send(dataWithoutDate);
+		expect(response.statusCode).toBe(400);
+
+		expect(response.body).toStrictEqual({
+			error: `Missing required property`,
+		});
+	});
+
+	test('It should catch invalid dates', async () => {
+		const response = await request(app).post('/launches').send(dataWithoutDate);
+		expect(response.statusCode).toBe(400);
+
+		expect(response.body).toStrictEqual({
+			error: `Launch date format is invalid`,
+		});
+	});
+});
