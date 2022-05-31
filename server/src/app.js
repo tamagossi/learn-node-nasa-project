1438662b8a0f44206b11ac21d3a16528e1b3b7e3
Raka Pratama <mgf.prauliyatama@gmail.com>
2022-05-31 10:08:56 +0700

    feat(): move API to have versioning

--- server/src/app.js
+++ server/src/app.js
@@ -16,7 +16,7 @@ app.use(
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.use(router);
+app.use('/v1', router);
 app.get('/*', (req, res) => {
 	res.sendFile(path.join(__dirname, '../public/', 'index.html'));
 });
